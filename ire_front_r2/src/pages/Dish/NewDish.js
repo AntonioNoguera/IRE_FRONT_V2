2dde952de3bcc71baf24d0a68a70b02df865f475
Michael Noguera <michael.noguerag@uanl.edu.mx>
2024-05-22 09:17:46 -0600

    Feat ( New Dish  ) ->

--- ire_front_r2/src/pages/Dish/NewDish.js
+++ ire_front_r2/src/pages/Dish/NewDish.js
@@ -12,8 +12,26 @@ import WhiteDummySpacer from "../../components/Layouts/WhiteDummySpacer";
 
 
 import  MotionImplementation  from './../../components/Layouts/MotionImplementation';
+import { useState } from "react";
 
 const NewDish = () => {
+    const [dishName, setDishName ] = useState('');
+    const [dishTemperature,setDishTemperature] = useState ('')
+
+    const [dishType, setDishType ] = useState('');
+    const [dishSauce, setDishSauce ] = useState('');
+    const [dishComplement, setDishComplement ] = useState('');
+    const [dishProtein, setDishProtein ] = useState(''); 
+
+    const extraObject = JSON.parse(localStorage.getItem('extras')) || [];
+
+    const temperatureOptions = [
+        { name : "Frío" , value : "Frío" },
+        { name : "Caliente" , value : "Caliente" },
+        { name : "Irrelevante" , value : "Irrelevante" },
+    ]
+
+
     return (
         <MotionImplementation verticalCentered = 'enabled'>
 
@@ -21,32 +39,55 @@ const NewDish = () => {
             <Title> Agregar Ingrediente </Title>
 
                 <Label>Escribe el nombre del platillo:</Label>
-                <EditText>Ingresa el nombre del grupo</EditText>
+                <EditText
+                    onChange = { e => setDishName(e.target.value)}
+                    previousValue= {dishName}
+                    placeholder = "Ingresa el nombre del platillo"/>
                 
                 <Label>Selecciona la temperatura: </Label>
-                <DropDownSelection>Ingresa el nombre del grupo</DropDownSelection>
+                <DropDownSelection
+                    onChange = { e => setDishTemperature(e.target.value)}
+                    selectedOption = {dishTemperature}
+                    optionsAvailable = {temperatureOptions}
+                    placeHolder = "Ingresa la temperatura del platillo"/>
 
                 <Label>Selecciona el tipo de platillo:</Label>
-                <DropDownSelection>Ingresa el nombre del grupo</DropDownSelection>
+                <DropDownSelection
+                    onChange = { e => setDishType(e.target.value)}
+                    selectedOption = {dishType}
+                    optionsAvailable={[]}
+                    placeholder = "Ingresa el tipo del platillo"/>
 
                 <HorizontalDisplay>
                     <CenteredDisplay width="100%">
                         <Label>Proteína:</Label> 
-                        <DropDownSelection>Proteinas disponibles</DropDownSelection>
+                        <DropDownSelection
+                            onChange = { e => setDishProtein(e.target.value)}
+                            selectedOption = {dishProtein}
+                            optionsAvailable={[]}
+                            placeholder = "Ingresa la proteína del platillo"/>
                     </CenteredDisplay>
                     
                     <WhiteDummySpacer/>
                     
                     <CenteredDisplay width="100%">
                         <Label>Acompañamiento:</Label> 
-                        <DropDownSelection>Acompañamientos disponibles</DropDownSelection>
+                        <DropDownSelection
+                            onChange = { e => setDishComplement(e.target.value)}
+                            selectedOption = {dishComplement}
+                            optionsAvailable={[]}
+                            placeholder = "Ingresa el acompañamiento del platillo"/>
                     </CenteredDisplay>
 
                     <WhiteDummySpacer/>
 
                     <CenteredDisplay width="100%">
                         <Label>Salsa:</Label> 
-                        <DropDownSelection>Salsas Disponibles</DropDownSelection>
+                        <DropDownSelection
+                            onChange = { e => setDishSauce(e.target.value)}
+                            previousValue= {dishSauce}
+                            optionsAvailable={[]}
+                            placeholder = "Ingresa la salsa del platillo"/> 
                     </CenteredDisplay>
                 </HorizontalDisplay>
  
